fb6e2f61db86a67e81dfb3a02c7e37acb5b80d7f
tonychyan <junqian@uw.edu>
2013-11-12 13:35:22 -0800

    4th iteration of UI

---
+++ Chart_2_files/axurerp_pagespecificscript.js
@@ -0,0 +1,8 @@
+﻿for(var i = 0; i < 10; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u0'] = 'top';HookHover('u1', false);
+gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u3'] = 'top';
